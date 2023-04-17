0bc60c76bd984ce85cb8c8bafea15eae35833392
Dimitry995 <98602983+Dimitry995@users.noreply.github.com>
2023-04-17 20:42:34 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,48 +1,4 @@
-//const fs = require('fs').promises;
-//const path = require('path').promises;
-//const info = msg => {
-//    console.log(`Info: ${msg}`);
-//  };
-//  
-//  const log = msg => {
-//    console.log(`Log: ${msg}`);
-//  };
-//  
-//  module.exports = {
-//    info,
-//    log,
-//  };
-// let contactsPath = require('./db/contacts.json');
-// function listContacts() {
-//    contactsPath.map(users => user {
-//        return (
-//            <section className="list">
-//            <ul>
-//            <li>
-//            <h1>{user.id}</h1>
-//            <h2>{user.name}</h2>
-//            <h3>{user.email}</h3>
-//            <h4>{user.phone}</h4>
-//            </li>
-//            </ul>
-//            </section>
-//        )
-//    })
-//  }
-//  
-//  function getContactById(contactId) {
-//    // ...твой код
-//  }
-//  
-//  function removeContact(contactId) {
-//    // ...твой код
-//  }
-//  
-//  function addContact(name, email, phone) {
-//    // ...твой код
-//  }
-//
-  const fs = require("fs/promises");
+const fs = require("fs/promises");
 const { contactsPath } = require("./db");
 const { nanoid } = require("nanoid");
 
